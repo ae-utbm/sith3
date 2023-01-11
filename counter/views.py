72d9eedf197d30dead6cfa34ae46d4dc1707c0ba
Thomas Girod <thgirod@hotmail.com>
2023-01-11 23:27:30 +0100

    Rajout d'un import qui s'était perdu

--- counter/views.py
+++ counter/views.py
@@ -23,6 +23,7 @@
 #
 
 from django.contrib.auth.decorators import login_required
+from django.db.models import F
 from django.shortcuts import get_object_or_404
 from django.http import Http404
 from django.core.exceptions import PermissionDenied
